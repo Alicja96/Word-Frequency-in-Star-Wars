2bd456aea085f2c70030eae9ac422e90badcd77d
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-07-04 13:11:48 +0200

    Update Web_Scraping_Transcripts.py

--- Web_Scraping_Transcripts.py
+++ Web_Scraping_Transcripts.py
@@ -8,11 +8,10 @@ urls = ['https://transcripts.fandom.com/wiki/Star_Wars_Episode_IV:_A_New_Hope',
         'https://transcripts.fandom.com/wiki/Star_Wars_Episode_I:_The_Phantom_Menace',  
         'https://transcripts.fandom.com/wiki/Star_Wars_Episode_II:_Attack_of_the_Clones',  
         'https://transcripts.fandom.com/wiki/Star_Wars_Episode_III:_Revenge_of_the_Sith',  
-        'https://transcripts.fandom.com/wiki/Star_Wars_Episode_VII:_The_Force_Awakens',
         'https://transcripts.fandom.com/wiki/Star_Wars_Episode_VIII:_The_Last_Jedi', 
         'https://transcripts.fandom.com/wiki/Star_Wars_Episode_IX:_The_Rise_of_Skywalker'] 
 
-movies = ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi', 'The Rise of Skywalker']
+movies = ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Last Jedi', 'The Rise of Skywalker']
 
 
 
@@ -29,7 +28,21 @@ transcripts = [get_transcripts(u) for u in urls]
 
 df = pd.DataFrame({'title': movies, 'transcript':transcripts})
 
-df['transcript'] = [''.join(map(str, t)) for t in df['transcript']]  # converting transcripts from lists to strings
+
+# web scraping transcript for The Force Awakens 
+
+url = 'https://transcripts.fandom.com/wiki/Star_Wars_Episode_VII:_The_Force_Awakens'
+page = requests.get(url).text
+soup = BeautifulSoup(page, "lxml")
+text = [pre.text for pre in soup.find_all('pre')]
+
+# adding force awakens transcript 
+
+df = df.append({'title' : 'The Force Awakens' , 'transcript' : text} , ignore_index=True)
+
+# converting transcripts from lists to strings
+
+df['transcript'] = [''.join(map(str, t)) for t in df['transcript']]  
 
 df.to_csv('star_wars_transcripts.csv',index=False)
 
