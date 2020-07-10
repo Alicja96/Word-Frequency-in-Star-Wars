56b1ac372572d23f95aacdcb759ef7cef90ee195
Alicja <57335001+Alicja96@users.noreply.github.com>
2020-07-10 13:45:51 +0200

    Update WebScrapingTranscripts.py

--- WebScrapingTranscripts.py
+++ WebScrapingTranscripts.py
@@ -13,36 +13,28 @@ urls = ['https://transcripts.fandom.com/wiki/Star_Wars_Episode_IV:_A_New_Hope',
 
 movies = ['A New Hope', 'The Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Last Jedi', 'The Rise of Skywalker']
 
-
-
 def get_transcripts(url): 
     
     '''Returns transcript from transcripts.fandom.com.'''
-    
     page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     text = [p.text for p in soup.find_all('p')]
     return text
 
-transcripts = [get_transcripts(u) for u in urls]
+transcripts = [get_transcripts(url) for url in urls]
 
 df = pd.DataFrame({'title': movies, 'transcript':transcripts})
 
-
 # web scraping transcript for The Force Awakens 
-
 url = 'https://transcripts.fandom.com/wiki/Star_Wars_Episode_VII:_The_Force_Awakens'
 page = requests.get(url).text
 soup = BeautifulSoup(page, "lxml")
 text = [pre.text for pre in soup.find_all('pre')]
 
 # adding force awakens transcript 
-
 df = df.append({'title' : 'The Force Awakens' , 'transcript' : text} , ignore_index=True)
 
 # converting transcripts from lists to strings
-
-df['transcript'] = [''.join(map(str, t)) for t in df['transcript']]  
+df['transcript'] = [''.join(map(str, transcript)) for transcript in df['transcript']]  
 
 df.to_csv('star_wars_transcripts.csv',index=False)
-
